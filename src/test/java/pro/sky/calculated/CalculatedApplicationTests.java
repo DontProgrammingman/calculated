ea41946fffbde216d86b0cfba7c3ec965ffbd2ae
serge <clonsfer@gmail.com>
2022-09-29 19:27:27 +0300

    initial commit

---
+++ src/test/java/pro/sky/calculated/CalculatedApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.calculated;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
